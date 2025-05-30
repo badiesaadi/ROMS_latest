db5a87f4a5931e7dfe5f32ccf238fe883140cae2
ilyes <benmaltiilyes@gmail.com>
2025-05-30 06:51:47 +0200

    improve comments; clean up unused code in WelcomeController

--- src/StageManager.java
+++ src/StageManager.java
@@ -1,38 +1,46 @@
 import javafx.stage.Stage;
 
 public class StageManager {
-    private static Stage primaryStage;
-    private static boolean isMaximized;
-    private static double width;
-    private static double height;
+    private static Stage primaryStage; 
+    private static boolean isMaximized; 
+    private static double width; 
+    private static double height; 
 
+    /**
+    initializes the StageManager with the given stage and
+      Sets up listeners to track changes in the stage's maximized state, width, and height.
+     */
     public static void initialize(Stage stage) {
-        primaryStage = stage;
-        isMaximized = stage.isMaximized();
-        width = stage.getWidth();
-        height = stage.getHeight();
+        primaryStage = stage; 
+        isMaximized = stage.isMaximized(); 
+        width = stage.getWidth(); 
+        height = stage.getHeight(); 
 
         stage.maximizedProperty().addListener((obs, oldVal, newVal) -> {
             isMaximized = newVal;
         });
 
         stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            if (!stage.isMaximized()) {
+            if (!stage.isMaximized()) { // Only update width if the stage is not maximized
                 width = newVal.doubleValue();
             }
         });
 
         stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            if (!stage.isMaximized()) {
+            if (!stage.isMaximized()) { 
                 height = newVal.doubleValue();
             }
         });
     }
 
+    /**
+     * Applies the saved stage settings (maximized state, width, and height) to the given stage.
+          */
     public static void applyStageSettings(Stage stage) {
         if (isMaximized) {
             stage.setMaximized(true);
         } else {
+            // Restore width and height if not maximized
             stage.setWidth(width);
             stage.setHeight(height);
         }
